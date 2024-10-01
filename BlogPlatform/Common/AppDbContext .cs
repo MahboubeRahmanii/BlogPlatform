3a846aadc6c58bbf093278d0d16402a9e56ab823
Mahboube <mahbouberahmani2@gmial.com>
2024-10-01 17:52:53 +0330

    Install SQlite

---
+++ BlogPlatform/Common/AppDbContext .cs
@@ -0,0 +1,20 @@
+﻿using BlogPlatform.Features.Comments;
+using BlogPlatform.Features.Posts;
+using BlogPlatform.Features.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPlatform.Common
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<PostVersion> PostVersions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=blogplatform.db");
+        }
+    }
+}
---
+++ BlogPlatform/Common/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogPlatform.Common.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureDbContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=blogplatform.db"));
+
+            return services;
+        }
+    }
+}
